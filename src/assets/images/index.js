547cff501c224216b692fa988cf925675c4cb6cd
srishti-maurya <srishti22maurya@gmail.com>
2022-10-10 17:37:01 +0530

    refactor: added assests

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+import burger from './burger.png';
+import coffee from './coffee.png';
+import food from './food.png';
+import pancake from './pancake.png';
+
+export { burger, coffee, food, pancake };
